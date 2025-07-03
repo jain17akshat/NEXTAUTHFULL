a1bb7ec1acabe0efd9871b6a45db2bc5297b04c8
jain17akshat <jain.7akshat@gmail.com>
2025-07-03 11:09:36 +0530

    Implement User schema with password hashing and timestamps

--- models/User.ts
+++ models/User.ts
@@ -7,3 +7,23 @@ export interface IUser{
     createdAt?:Date;
     updatedAt?:Date;
 }
+const UserSchema=new Schema<IUser>(
+    {
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true}
+    },
+    {
+        timestamps:true,
+    }
+
+);
+userSchema.pre('save',async function(next){
+    if(this.isModified("password")){
+this.password=await bcrypt.hash(this.password,10)
+
+
+}
+
+next()
+
+})
