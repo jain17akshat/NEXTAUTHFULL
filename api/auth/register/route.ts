6a807dd253aea2330cdda053b5980f88a51b694d
jain17akshat <jain.7akshat@gmail.com>
2025-07-05 14:25:54 +0530

    Add registration route with email and password validation

---
+++ api/auth/register/route.ts
@@ -0,0 +1,23 @@
+  import {connectToDatabase} from '@lib/db'
+  import User from "@/models/User"
+  import {NextRequest,NextResponse} from "next/server"
+
+  export async function POST(request:NextRequest) 
+  {
+  try {
+const {email ,password}=await request.json()
+if(!email||!password){
+    return NextResponse.json(
+        {error:"Email and password are required"},
+        {status:400}
+    )
+}
+
+
+
+
+
+} catch (error) {
+    
+  }  
+  }
